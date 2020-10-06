14967fb08ab4710826ef964ead1a0f3b5a4e9919
Vitória Guardieiro <viigua@gmail.com>
2020-10-06 15:35:16 -0300

    Modify gen_pickle to generate LBP only from faces

--- gen_pickle.py
+++ gen_pickle.py
@@ -1,11 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # Library
-
-# In[18]:
-
-
 # import the necessary packages
 from localbinarypatterns import LocalBinaryPatterns
 from imutils import paths
@@ -15,11 +7,36 @@ import cv2
 import os
 import pickle
 
+def image_resize(image, width = None, height = None, inter = cv2.INTER_AREA):
+    # initialize the dimensions of the image to be resized and
+    # grab the image size
+    dim = None
+    (h, w) = image.shape[:2]
 
-# # Initialize variables
+    # if both the width and height are None, then return the
+    # original image
+    if width is None and height is None:
+        return image
 
-# In[12]:
+    # check to see if the width is None
+    if width is None:
+        # calculate the ratio of the height and construct the
+        # dimensions
+        r = height / float(h)
+        dim = (int(w * r), height)
 
+    # otherwise, the height is None
+    else:
+        # calculate the ratio of the width and construct the
+        # dimensions
+        r = width / float(w)
+        dim = (width, int(h * r))
+
+    # resize the image
+    resized = cv2.resize(image, dim, interpolation = inter)
+
+    # return the resized image
+    return resized
 
 # initialize the local binary patterns descriptor along with
 # the data and label lists
@@ -29,47 +46,57 @@ labels_real = []
 data_fake = []
 labels_fake = []
 
-
-# # Loop over real and fake images
-
-# In[13]:
-
+detector = cv2.CascadeClassifier("face_detector/haarcascade_frontalface_default.xml")
 
 # loop over the real images
 for imagePath in paths.list_images("frames_reais/"):
 	# load the image, convert it to grayscale, and describe it
 	image = cv2.imread(imagePath)
 	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-	hist = desc.describe(gray)
-	# extract the label from the image path, then update the
-	# label and data lists
-	im = imagePath.split(os.path.sep)[-1]
-	im = im[0:4]
-	labels_real.append(im)
-	data_real.append(hist)
+	# detect face
+	rects = detector.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30),
+									flags=cv2.CASCADE_SCALE_IMAGE)
+	# for each face detected
+	for rect in rects:
+		# get only the image of the face
+		face_gray = gray[rect[1]:rect[1]+rect[3], rect[0]:rect[0]+rect[2]]
+		pil_gray = Image.fromarray(face_gray)
+		open_cv_image = np.array(pil_gray)
+	
+		open_cv_image = image_resize(open_cv_image, height = 150)
 
-
-# In[16]:
-
+		hist = desc.describe(open_cv_image)
+		# extract the label from the image path, then update the
+		# label and data lists
+		im = imagePath.split(os.path.sep)[-1]
+		im = im[0:4]
+		labels_real.append(im)
+		data_real.append(hist)
 
 # loop over fake images
 for imagePath in paths.list_images("frames_fakes/"):
 	# load the image, convert it to grayscale, and describe it
 	image = cv2.imread(imagePath)
 	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-	hist = desc.describe(gray)
-	# extract the label from the image path, then update the
-	# label and data lists
-	im = imagePath.split(os.path.sep)[-1]
-	im = im[0:4]
-	labels_fake.append(im)
-	data_fake.append(hist)
+		# detect face
+	rects = detector.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30),
+									flags=cv2.CASCADE_SCALE_IMAGE)
+	# for each face detected
+	for rect in rects:
+		# get only the image of the face
+		face_gray = gray[rect[1]:rect[1]+rect[3], rect[0]:rect[0]+rect[2]]
+		pil_gray = Image.fromarray(face_gray)
+		open_cv_image = np.array(pil_gray)
+	
+		open_cv_image = image_resize(open_cv_image, height = 150)
 
-
-# # Split Data
-
-# In[19]:
-
+		hist = desc.describe(open_cv_image)
+		# extract the label from the image path, then update the
+		# label and data lists
+		im = imagePath.split(os.path.sep)[-1]
+		im = im[0:4]
+		labels_fake.append(im)
+		data_fake.append(hist)
 
 # Split and join data
 X_train_real, X_test_real, y_train_real, y_test_real = train_test_split(data_real, labels_real,
@@ -80,12 +107,6 @@ X_train = X_train_real + X_train_fake
 X_test = X_test_real + X_test_fake
 y_train = y_train_real + y_train_fake
 y_test = y_test_real + y_test_fake
-
-
-# # Save data pickle
-
-# In[29]:
-
 
 # Save data
 pickle_out = open("data/data_train.pickle","wb")
@@ -103,4 +124,3 @@ pickle_out.close()
 pickle_out = open("data/labels_test.pickle","wb")
 pickle.dump(y_test, pickle_out)
 pickle_out.close()
-
